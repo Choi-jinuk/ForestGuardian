982b396a79c0a4d040e7ee1b5ff6eff5637d4cee
Choi-jinuk <wlsdnr7536@naver.com>
2021-07-25 12:13:28 +0900

    Project upload-5

---
+++ Forest_Guardian/Assets/08.Assets/Map/2D_Jungle/Shaders/Waterfall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waterfall : MonoBehaviour 
+{
+
+	public float speed = 0;
+
+    private void Start()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().sortingLayerName = "Background5";
+    }
+
+    void Update () 
+	{
+	
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (0,Time.time * speed);
+	}
+}
